89febe4d8d38e253af41a1597ee41ac1903565e1
hanyixiao <2456181875@qq.com>
2020-04-21 11:34:28 +0800

    create the project

---
+++ 1.0/ARM_v7_ASM/bubble_sort.c
@@ -0,0 +1,28 @@
+/********************************************************************************
+ * bublle_sort.c
+ * Create by hanyixiao on 2020 4 20
+ * Copyright (c) 2020 yixiaoHan.  All rights reserved.
+ * hanyixiao@github.com
+ * target arm-v7a rasbberypi-4B
+ *******************************************************************************/      
+#include <stdio.h>
+#define N 32768
+int data[N];
+int main(int argc,char **argv)
+{
+    int temp;
+    int i,j;
+    for(int i=0;i<N-1;i++)
+    {
+        for(int j=0;j<N-1;j++)
+        {
+            if(data[j]<data[j+1])
+            {
+                temp=data[j];
+                data[j]=data[j+1];
+                data[j+1]=temp;
+            }
+        }
+    }  
+    return 0;  
+}
