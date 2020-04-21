7430e6d03344f243d233668f27f3cbce0d37d408
hanyixiao <2456181875@qq.com>
2020-04-21 11:54:51 +0800

    add makefile

--- 1.0/ARM_v7_ASM/bubble_sort_asm.s
+++ 1.0/ARM_v7_ASM/bubble_sort_asm.s
@@ -14,7 +14,7 @@ main:   ldr r1,=N
         sub r1,r1,#1 @r1 = N-1
         mov r5,#0    @i=0
 oloop:  cmp r5,r1    @i==N-1?
-        beq extio
+        beq exito
         mov r3,#0    @j=0
         ldr r2,=data @r2=data
         ldr r11,[r2] 
